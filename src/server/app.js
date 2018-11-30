57c5068c4476d2583091e2c1bd8096e68834e7db
artrnh <artrnh@gmail.com>
2018-11-30 03:00:23 +0300

    deploy fix #4

--- src/server/app.js
+++ src/server/app.js
@@ -5,4 +5,4 @@ const app = express();
 app.use(express.static('public'));
 app.get('/api/getTestData', (req, res) => res.json('Test!'));
 
-app.listen(8080, () => console.log('Listening on port 8080!'));
+app.listen(process.env.PORT || 8080, () => console.log('Listening on port 8080!'));
